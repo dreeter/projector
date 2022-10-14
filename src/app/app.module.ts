6556bb70cdb633fc711e9576a08f7458ad60589f
Derek Reeter <dreeter00@gmail.com>
2022-10-13 19:24:50 -0600

    Initial Commit - basic proof of concept for Home Page - basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,83 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule, Routes } from '@angular/router';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { ActionListComponent } from './action-list/action-list.component';
+import { HomeComponent } from './home/home.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { ProjectItemComponent } from './project-item/project-item.component';
+import { ProjectComponent } from './project/project.component';
+import { TaskTableComponent } from './task-table/task-table.component';
+import { TaskComponent } from './task/task.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import {
+  MatFormFieldModule,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+} from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: 'newproject', component: NewProjectComponent, pathMatch: 'full' },
+  { path: 'newtask', component: NewTaskComponent, pathMatch: 'full' },
+  { path: 'project', component: ProjectComponent, pathMatch: 'full' },
+  { path: 'task', component: ProjectComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    NewProjectComponent,
+    ActionListComponent,
+    HomeComponent,
+    NewTaskComponent,
+    ProjectItemComponent,
+    ProjectComponent,
+    TaskTableComponent,
+    TaskComponent,
+    TaskItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatTableModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    RouterModule.forRoot(appRoutes),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'fill', floatLabel: 'always' },
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
